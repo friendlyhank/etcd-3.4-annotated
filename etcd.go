7423b10abd9051ed85e917f44d65ce6d3be5b8ce
Xiang Li <xiang.li@coreos.com>
2013-07-26 12:15:42 -0700

    change init func due to the change in raft

--- etcd.go
+++ etcd.go
@@ -225,9 +225,10 @@ func startRaft(securityType int) {
 		}
 	}
 
-	raftServer.Initialize()
 	raftServer.SetElectionTimeout(ELECTIONTIMTOUT)
 	raftServer.SetHeartbeatTimeout(HEARTBEATTIMEOUT)
+
+	raftServer.Start()
 
 	// start to response to raft requests
 	go startRaftTransport(info.RaftPort, securityType)
@@ -236,7 +237,6 @@ func startRaft(securityType int) {
 
 		// start as a leader in a new cluster
 		if len(cluster) == 0 {
-			raftServer.StartLeader()
 
 			time.Sleep(time.Millisecond * 20)
 
@@ -256,7 +256,6 @@ func startRaft(securityType int) {
 
 			// start as a follower in a existing cluster
 		} else {
-			raftServer.StartFollower(false)
 
 			time.Sleep(time.Millisecond * 20)
 
@@ -291,7 +290,6 @@ func startRaft(securityType int) {
 
 	} else {
 		// rejoin the previous cluster
-		raftServer.StartFollower(true)
 		debug("%s restart as a follower", raftServer.Name())
 	}
 
